c8ad72ce2fdb7c448e0a06c294b25a75add6c7a4
조영무 <fprh13@gmail.com>
2025-02-07 18:31:35 +0900

    chore: 에러 코드 추가 #2

--- src/main/java/project/mapjiri/global/exception/MyErrorCode.java
+++ src/main/java/project/mapjiri/global/exception/MyErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum MyErrorCode {
     // 예시
     COURSE_CANNOT_BE_DELETED(HttpStatus.BAD_REQUEST, "수강생이 있어 강좌를 삭제할 수 없습니다."),
-
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 오류가 발생했습니다."),
+    BAD_GATEWAY(HttpStatus.BAD_GATEWAY, "외부 서비스에서 오류가 발생했습니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     ;
 
     private final HttpStatus status;
