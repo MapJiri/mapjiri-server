fd3077a3ebdf57e456d387eb12ea3a8461bd581c
hyunoi <nhe0622@gmail.com>
2025-02-11 16:53:49 +0900

    feat: UserController signUp 메소드 생성

---
+++ src/main/java/project/mapjiri/domain/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package project.mapjiri.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.mapjiri.domain.user.dto.request.SignUpRequestDto;
+import project.mapjiri.domain.user.dto.response.SignUpResponseDto;
+import project.mapjiri.domain.user.service.UserService;
+
+import javax.naming.AuthenticationException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto requestDto) throws AuthenticationException {
+        SignUpResponseDto response = userService.signUp(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
