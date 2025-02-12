eaf6308deff8baa7167547e1dadd0745ff4c42ad
hyunoi <nhe0622@gmail.com>
2025-02-12 15:51:36 +0900

    feat: RefreshAccessToken Request, Response Dto 생성

---
+++ src/main/java/project/mapjiri/domain/user/dto/response/RefreshAccessTokenResponseDto.java
@@ -0,0 +1,13 @@
+package project.mapjiri.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshAccessTokenResponseDto {
+    private String accessToken;
+    private String refreshToken;
+}
