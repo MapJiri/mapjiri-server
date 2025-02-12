d31112f643caafc25227e1c21e5271426d62e7c7
hyunoi <nhe0622@gmail.com>
2025-02-12 16:30:39 +0900

    comment: redisService 주석 추가

--- src/main/java/project/mapjiri/domain/user/service/RedisService.java
+++ src/main/java/project/mapjiri/domain/user/service/RedisService.java
@@ -12,21 +12,22 @@ public class RedisService {
     private final RedisTemplate<String, String> redisTemplate;
     private final long REFRESH_TOKEN_EXPIRATION = 7 * 24 * 60 * 60 * 1000;      // 7일
 
-    // ✅ Refresh Token 저장
+    // RefreshToken 저장
     public void setRefreshToken(String email, String refreshToken) {
         redisTemplate.opsForValue().set(email, refreshToken, REFRESH_TOKEN_EXPIRATION, TimeUnit.SECONDS);
     }
 
-    // ✅ Refresh Token 조회 (재발급 시 사용)
+    // RefreshToken 조회
     public String getRefreshToken(String email) {
         return redisTemplate.opsForValue().get(email);
     }
 
-    // ✅ Refresh Token 삭제 (로그아웃 시 사용)
+    // RefreshToken 삭제
     public void deleteRefreshToken(String email) {
         redisTemplate.delete(email);
     }
 
+    // AccessToken 블랙 리스트 저장
     public void addToBlacklist(String accessToken) {
         redisTemplate.opsForValue().set(accessToken, "BLACKLIST", REFRESH_TOKEN_EXPIRATION, TimeUnit.SECONDS);
     }
