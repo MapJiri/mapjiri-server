ada4a5e27fcf373f65514f350c423b757cde61a4
hyunoi <nhe0622@gmail.com>
2025-02-12 11:17:08 +0900

    feat: existsByEmail, existsByUsername 추가

--- src/main/java/project/mapjiri/domain/user/repository/UserRepository.java
+++ src/main/java/project/mapjiri/domain/user/repository/UserRepository.java
@@ -6,5 +6,8 @@ import project.mapjiri.domain.user.model.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+
     Optional<User> findByEmail(String email);
 }
