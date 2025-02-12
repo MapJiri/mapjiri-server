fbe31f5ba4b203a3e017c1e889a339b0acc64f68
hyunoi <nhe0622@gmail.com>
2025-02-12 15:50:49 +0900

    feat: UserController refreshAccessToken 생성

--- src/main/java/project/mapjiri/domain/user/controller/UserController.java
+++ src/main/java/project/mapjiri/domain/user/controller/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.mapjiri.domain.user.dto.request.RefreshAccessTokenRequestDto;
 import project.mapjiri.domain.user.dto.request.SignInRequestDto;
 import project.mapjiri.domain.user.dto.request.SignUpRequestDto;
+import project.mapjiri.domain.user.dto.response.RefreshAccessTokenResponseDto;
 import project.mapjiri.domain.user.dto.response.SignInResponseDto;
 import project.mapjiri.domain.user.dto.response.SignUpResponseDto;
 import project.mapjiri.domain.user.service.UserService;
@@ -30,6 +32,12 @@ public class UserController {
     @PostMapping("/signin")
     public ResponseEntity<SignInResponseDto> signIn(@RequestBody SignInRequestDto request) throws AuthenticationException {
         SignInResponseDto response = userService.signIn(request);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("/access-token")
+    public ResponseEntity<RefreshAccessTokenResponseDto> refreshAccessToken(@RequestBody RefreshAccessTokenRequestDto request) throws AuthenticationException {
+        RefreshAccessTokenResponseDto response = userService.refreshAccessToken(request);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
