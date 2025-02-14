11f6aae1930664c3d18480c4b5c9d5a3722db7b0
SD-gif <alem6516@naver.com>
2025-02-14 14:23:40 +0900

    feat : 메뉴 즐겨찾기 기능 활성화

--- src/main/java/project/mapjiri/domain/menustar/service/MenuStarService.java
+++ src/main/java/project/mapjiri/domain/menustar/service/MenuStarService.java
@@ -1,60 +1,61 @@
-//package project.mapjiri.domain.menustar.service;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import project.mapjiri.domain.menu.model.Menu;
-//import project.mapjiri.domain.menustar.dto.MenuStarRequest;
-//import project.mapjiri.domain.menustar.model.MenuStar;
-//import project.mapjiri.domain.menustar.repository.MenuStarRepository;
-//import project.mapjiri.domain.user.model.User;
-//import project.mapjiri.global.dto.ResponseDto;
-//import project.mapjiri.global.exception.MyErrorCode;
-//import project.mapjiri.global.exception.MyException;
-//
-//import java.util.List;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class MenuStarService {
-//
-//    private final UserSerivce userService;
-//    private final MenuService menuService;
-//    private MenuStarRepository menuStarRepository;
-//
-//    public ResponseDto<Long> addMenuStar(MenuStarRequest request){
-//
-//        User user = userService.findUser();
-//        Menu menu = menuService.findMenu(request.getMenuKeyword());
-//
-//        if (menuStarRepository.existsByUserAndMenu(user, menu)) {
-//            throw new MyException(MyErrorCode.ALREADY_FAVORITE_MENU);
-//        }
-//
-//        MenuStar menuStar = MenuStar.of(user, menu);
-//        menuStarRepository.save(menuStar);
-//
-//        return ResponseDto.of(menu.getMenuId(), "메뉴 즐겨찾기 추가 성공");
-//    }
-//
-//    public ResponseDto<List<MenuStar>> getMenuStar(){
-//
-//        User user = userService.findUser();
-//
-//        List<MenuStar> menuStarList = menuStarRepository.findByUser(user);
-//
-//        return ResponseDto.of(menuStarList,"메뉴 즐겨찾기 조회 성공");
-//    }
-//
-//    // 즐겨찾기 취소 기능
-//    public ResponseDto<Void> delMenuStar(String menuKeyword) {
-//
-//        User user = userService.findUser();
-//        Menu menu = menuService.findMenu(menuKeyword);
-//
-//        MenuStar menuStar = menuStarRepository.findByUserAndMenu(user, menu)
-//                .orElseThrow(() -> new MyException(MyErrorCode.NOT_FOUND_FAVORITE_MENU));
-//        menuStarRepository.delete(menuStar);
-//
-//        return ResponseDto.of(null, "즐겨찾기 취소 성공");
-//    }
-//}
+package project.mapjiri.domain.menustar.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.mapjiri.domain.menu.model.Menu;
+import project.mapjiri.domain.menu.service.MenuService;
+import project.mapjiri.domain.menustar.dto.MenuStarRequest;
+import project.mapjiri.domain.menustar.model.MenuStar;
+import project.mapjiri.domain.menustar.repository.MenuStarRepository;
+import project.mapjiri.domain.user.model.User;
+import project.mapjiri.domain.user.service.UserService;
+import project.mapjiri.global.exception.MyErrorCode;
+import project.mapjiri.global.exception.MyException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MenuStarService {
+
+    private final UserService userService;
+    private final MenuService menuService;
+    private final MenuStarRepository menuStarRepository;
+
+    public MenuStar addMenuStar(MenuStarRequest request){
+
+        User user = userService.findUser();
+        Menu menu = menuService.findMenu(request.getMenuKeyword());
+
+        if (menuStarRepository.existsByUserAndMenu(user, menu)) {
+            throw new MyException(MyErrorCode.ALREADY_FAVORITE_MENU);
+        }
+
+        MenuStar menuStar = MenuStar.of(user, menu);
+        menuStarRepository.save(menuStar);
+
+        return menuStar;
+    }
+
+    public List<String> getMenuStar(){
+
+        User user = userService.findUser();
+        List<MenuStar> menuStarList = menuStarRepository.findByUser(user);
+
+        return menuStarList.stream()
+                .map(menuStar -> menuStar.getMenu().getMenuName()) // 메뉴 이름만 리스트로 변환
+                .collect(Collectors.toList());
+    }
+
+    // 즐겨찾기 취소 기능
+    public void delMenuStar(String menuKeyword) {
+
+        User user = userService.findUser();
+        Menu menu = menuService.findMenu(menuKeyword);
+
+        MenuStar menuStar = menuStarRepository.findByUserAndMenu(user, menu)
+                .orElseThrow(() -> new MyException(MyErrorCode.NOT_FOUND_FAVORITE_MENU));
+        menuStarRepository.delete(menuStar);
+    }
+}
