ca54b21152e6ce54491dcb9ee48a1d142d85de77
SD-gif <alem6516@naver.com>
2025-02-14 14:22:25 +0900

    feat : 위치 즐겨찾기 활성화, API 연동

--- src/main/java/project/mapjiri/domain/placeStar/controller/PlaceStarController.java
+++ src/main/java/project/mapjiri/domain/placeStar/controller/PlaceStarController.java
@@ -1,35 +1,39 @@
-//package project.mapjiri.domain.placeStar.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import project.mapjiri.domain.placeStar.dto.PlaceStarRequest;
-//import project.mapjiri.domain.placeStar.model.PlaceStar;
-//import project.mapjiri.domain.placeStar.service.PlaceStarService;
-//import project.mapjiri.global.dto.ResponseDto;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/v1/favorite/place")
-//public class PlaceStarController {
-//
-//    private final PlaceStarService placeStarService;
-//
-//    @PostMapping
-//    ResponseEntity<ResponseDto<Long>> addPlaceStar(@RequestBody PlaceStarRequest request){
-//        return ResponseEntity.status(HttpStatus.OK).body(placeStarService.addPlaceStar(request));
-//    }
-//
-//    @GetMapping
-//    ResponseEntity<ResponseDto<List<PlaceStar>>> getPlaceStar(){
-//        return ResponseEntity.status(HttpStatus.OK).body(placeStarService.getPlaceStar());
-//    }
-//
-//    @DeleteMapping
-//    ResponseEntity<ResponseDto<Void>> delPlaceStar(@RequestParam("placeKeyword") String placeKeyword){
-//        return ResponseEntity.status(HttpStatus.OK).body(placeStarService.delPlaceStar(placeKeyword));
-//    }
-//}
+package project.mapjiri.domain.placeStar.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.mapjiri.domain.placeStar.dto.PlaceStarRequest;
+import project.mapjiri.domain.placeStar.model.PlaceStar;
+import project.mapjiri.domain.placeStar.service.PlaceStarService;
+import project.mapjiri.global.dto.ResponseDto;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/star/place")
+public class PlaceStarController {
+
+    private final PlaceStarService placeStarService;
+
+    @PostMapping
+    ResponseEntity<ResponseDto<PlaceStar>> addPlaceStar(@RequestBody PlaceStarRequest request){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseDto.of(placeStarService.addPlaceStar(request),"즐겨찾기 추가 성공"));
+    }
+
+    @GetMapping
+    ResponseEntity<ResponseDto<List<String>>> getPlaceStar(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseDto.of(placeStarService.getPlaceStar(),"즐겨찾기 조회 성공"));
+    }
+
+    @DeleteMapping
+    ResponseEntity<ResponseDto<Void>> delPlaceStar(@RequestParam("placeKeyword") String placeKeyword){
+        placeStarService.delPlaceStar(placeKeyword);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseDto.of(null, "즐겨찾기 삭제 성공"));
+    }
+}
