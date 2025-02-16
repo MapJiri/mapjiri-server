30efda88863c944cfaaaa120a6e33dfff78cdb3a
hyunoi <nhe0622@gmail.com>
2025-02-17 01:48:21 +0900

    fix: 에러 코드들 MyErrorCode로 교체

--- src/main/java/project/mapjiri/global/exception/MyErrorCode.java
+++ src/main/java/project/mapjiri/global/exception/MyErrorCode.java
@@ -18,7 +18,17 @@ public enum MyErrorCode {
     NOT_FOUND_FAVORITE_MENU(HttpStatus.NOT_FOUND,"즐겨찾기에 등록되지 않은 메뉴 입니다." ),
     NOT_FOUND_PLACE(HttpStatus.NOT_FOUND,"등록된 위치가 없습니다."),
     NOT_FOUND_MENU(HttpStatus.NOT_FOUND,"등록된 메뉴가 없습니다."),
-    NOT_FOUND_CSVFILE(HttpStatus.NOT_FOUND,"CSV 파일을 찾을 수 없습니다.");
+    NOT_FOUND_CSVFILE(HttpStatus.NOT_FOUND,"CSV 파일을 찾을 수 없습니다."),
+    INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "인증 번호가 일치하지 않습니다."),
+    EXPIRED_OR_NOT_FOUND_VERIFICATION_CODE(HttpStatus.NOT_FOUND, "인증 번호가 만료되었거나 존재하지 않습니다."),
+    EMAIL_VERIFICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "이메일 인증이 필요합니다."),
+    ALREADY_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND,"존재하지 않는 이메일입니다."),
+    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 Refresh Token 입니다."),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "이미 만료된 Refresh Token 입니다."),
+    UNAUTHENTICATED_USER(HttpStatus.UNAUTHORIZED, "인증된 사용자가 없습니다.");
 
     private final HttpStatus status;
     private final String message;
