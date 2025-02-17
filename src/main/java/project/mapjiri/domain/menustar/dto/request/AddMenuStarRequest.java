cc69269d503ad2457877f6d96a34b3cd7b0022f4
SD-gif <alem6516@naver.com>
2025-02-17 18:34:14 +0900

    chore : 테스트 용 setter 추가

--- src/main/java/project/mapjiri/domain/menustar/dto/request/AddMenuStarRequest.java
+++ src/main/java/project/mapjiri/domain/menustar/dto/request/AddMenuStarRequest.java
@@ -2,8 +2,10 @@ package project.mapjiri.domain.menustar.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class AddMenuStarRequest {
 
--- src/main/java/project/mapjiri/domain/placeStar/dto/request/AddPlaceStarRequest.java
+++ src/main/java/project/mapjiri/domain/placeStar/dto/request/AddPlaceStarRequest.java
@@ -2,8 +2,10 @@ package project.mapjiri.domain.placeStar.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class AddPlaceStarRequest {
 
