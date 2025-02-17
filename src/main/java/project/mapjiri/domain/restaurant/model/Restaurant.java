23a3d409f8ae89b5b484cff3645d94d16997fa85
Kimseungin0529 <whffkaos007@naver.com>
2025-02-17 11:55:35 +0900

    refactor : Restaurant Menu 연관관계 필드 삭제

--- src/main/java/project/mapjiri/domain/restaurant/model/Restaurant.java
+++ src/main/java/project/mapjiri/domain/restaurant/model/Restaurant.java
@@ -1,10 +1,12 @@
 package project.mapjiri.domain.restaurant.model;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import project.mapjiri.domain.menu.model.Menu;
 import project.mapjiri.domain.review.model.ReviewTag;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -38,10 +40,6 @@ public class Restaurant {
 
     @Column(nullable = false)
     private Double restaurantLatitude;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "menu_id")
-    private Menu menu;
 
     public Restaurant(String restaurantName, String restaurantOldPlace, String restaurantNewPlace, String restaurantNumber, ReviewTag reviewTag, Double restaurantLongitude, Double restaurantLatitude) {
         this.restaurantName = restaurantName;
